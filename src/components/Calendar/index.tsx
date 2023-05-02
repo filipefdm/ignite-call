e6720c7d893a290ea0c472be56465529b7cf1919
Filipe <femotta96@gmail.com>
2023-05-02 12:52:39 -0300

    Calendário e disponibilidade - Navegando nos meses

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -8,22 +8,43 @@ import {
   CalendarHeader,
   CalendarTitle,
 } from "./styles";
+import { useState } from "react";
+import dayjs from "dayjs";
 
 export function Calendar() {
+  const [currentDate, setCurrentDate] = useState(() => {
+    return dayjs().set("date", 1);
+  });
+
+  function handlePreviousMonth() {
+    const previousMonthDate = currentDate.subtract(1, "month");
+
+    setCurrentDate(previousMonthDate);
+  }
+
+  function handleNextMonth() {
+    const previousMonthDate = currentDate.add(1, "month");
+
+    setCurrentDate(previousMonthDate);
+  }
+
   const shortWeekDays = getWeekDays({ short: true });
+
+  const currentMonth = currentDate.format("MMMM");
+  const currentYear = currentDate.format("YYYY");
 
   return (
     <CalendarContainer>
       <CalendarHeader>
         <CalendarTitle>
-          Janeiro <span>2023</span>
+          {currentMonth} <span>{currentYear}</span>
         </CalendarTitle>
 
         <CalendarActions>
-          <button>
+          <button onClick={handlePreviousMonth} title="Previous month">
             <CaretLeft />
           </button>
-          <button>
+          <button onClick={handleNextMonth} title="Next month">
             <CaretRight />
           </button>
         </CalendarActions>
