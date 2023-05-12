42377418cbf2da6bd46bf5dd03319368d5dd9a9b
Filipe <femotta96@gmail.com>
2023-05-12 11:38:30 -0300

    Calendário e disponibilidade - Alterando banco para MySQL

---
+++ prisma/migrations/20230511151921_add_database_indexes/migration.sql
@@ -0,0 +1,58 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_accounts" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "user_id" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "provider" TEXT NOT NULL,
+    "provider_account_id" TEXT NOT NULL,
+    "refresh_token" TEXT,
+    "access_token" TEXT,
+    "expires_at" INTEGER,
+    "token_type" TEXT,
+    "scope" TEXT,
+    "id_token" TEXT,
+    "session_state" TEXT
+);
+INSERT INTO "new_accounts" ("access_token", "expires_at", "id", "id_token", "provider", "provider_account_id", "refresh_token", "scope", "session_state", "token_type", "type", "user_id") SELECT "access_token", "expires_at", "id", "id_token", "provider", "provider_account_id", "refresh_token", "scope", "session_state", "token_type", "type", "user_id" FROM "accounts";
+DROP TABLE "accounts";
+ALTER TABLE "new_accounts" RENAME TO "accounts";
+CREATE INDEX "accounts_user_id_idx" ON "accounts"("user_id");
+CREATE UNIQUE INDEX "accounts_provider_provider_account_id_key" ON "accounts"("provider", "provider_account_id");
+CREATE TABLE "new_schedulings" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "date" DATETIME NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "observations" TEXT,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "user_id" TEXT NOT NULL
+);
+INSERT INTO "new_schedulings" ("created_at", "date", "email", "id", "name", "observations", "user_id") SELECT "created_at", "date", "email", "id", "name", "observations", "user_id" FROM "schedulings";
+DROP TABLE "schedulings";
+ALTER TABLE "new_schedulings" RENAME TO "schedulings";
+CREATE INDEX "schedulings_user_id_idx" ON "schedulings"("user_id");
+CREATE TABLE "new_sessions" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "session_token" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "expires" DATETIME NOT NULL
+);
+INSERT INTO "new_sessions" ("expires", "id", "session_token", "user_id") SELECT "expires", "id", "session_token", "user_id" FROM "sessions";
+DROP TABLE "sessions";
+ALTER TABLE "new_sessions" RENAME TO "sessions";
+CREATE UNIQUE INDEX "sessions_session_token_key" ON "sessions"("session_token");
+CREATE INDEX "sessions_user_id_idx" ON "sessions"("user_id");
+CREATE TABLE "new_user_time_intervals" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "week_day" INTEGER NOT NULL,
+    "time_start_in_minutes" INTEGER NOT NULL,
+    "time_end_in_minutes" INTEGER NOT NULL,
+    "user_id" TEXT NOT NULL
+);
+INSERT INTO "new_user_time_intervals" ("id", "time_end_in_minutes", "time_start_in_minutes", "user_id", "week_day") SELECT "id", "time_end_in_minutes", "time_start_in_minutes", "user_id", "week_day" FROM "user_time_intervals";
+DROP TABLE "user_time_intervals";
+ALTER TABLE "new_user_time_intervals" RENAME TO "user_time_intervals";
+CREATE INDEX "user_time_intervals_user_id_idx" ON "user_time_intervals"("user_id");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
--- src/pages/api/users/[username]/blocked-dates.api.ts
+++ src/pages/api/users/[username]/blocked-dates.api.ts
@@ -1,29 +1,29 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import { prisma } from '../../../../lib/prisma'
+import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../../../lib/prisma";
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse,
+  res: NextApiResponse
 ) {
-  if (req.method !== 'GET') {
-    return res.status(405).end()
+  if (req.method !== "GET") {
+    return res.status(405).end();
   }
 
-  const username = String(req.query.username)
-  const { year, month } = req.query
+  const username = String(req.query.username);
+  const { year, month } = req.query;
 
   if (!year || !month) {
-    return res.status(400).json({ message: 'Year or month not specified.' })
+    return res.status(400).json({ message: "Year or month not specified." });
   }
 
   const user = await prisma.user.findUnique({
     where: {
       username,
     },
-  })
+  });
 
   if (!user) {
-    return res.status(400).json({ message: 'User does not exist.' })
+    return res.status(400).json({ message: "User does not exist." });
   }
 
   const availableWeekDays = await prisma.userTimeInterval.findMany({
@@ -33,13 +33,20 @@ export default async function handler(
     where: {
       user_id: user.id,
     },
-  })
+  });
 
   const blockedWeekDays = [0, 1, 2, 3, 4, 5, 6].filter((weekDay) => {
     return !availableWeekDays.some(
-      (availableWeekDay) => availableWeekDay.week_day === weekDay,
-    )
-  })
+      (availableWeekDay) => availableWeekDay.week_day === weekDay
+    );
+  });
 
-  return res.json({ blockedWeekDays })
+  const blockedDatesRaw = await prisma.$queryRaw`
+    SELECT *
+    FROM schedulings S
+    WHERE S.user_id = ${user.id}
+      AND DATE_FORMAT(S.date, "%Y-%m") = ${`${year}-${month}`}
+  `;
+
+  return res.json({ blockedWeekDays, blockedDatesRaw });
 }
